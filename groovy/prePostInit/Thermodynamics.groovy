c365c657814bc668f668bd4f4730c3887806ebc3
Oganesson897 <oganesson897@gmail.com>
2025-06-06 08:17:58 +0900

    fix: output amount
    my eyes

--- groovy/prePostInit/Thermodynamics.groovy
+++ groovy/prePostInit/Thermodynamics.groovy
@@ -859,7 +859,7 @@ recipemap('heat_exchanger').recipeBuilder()
         .fluidInputs(liquid('water') * 96)
         .fluidInputs(liquid('lava') * 10)
         .fluidOutputs(liquid('steam') * 15360)
-        .fluidOutputs(liquid('chilled_lava') * 3)
+        .fluidOutputs(liquid('chilled_lava') * 10)
         .duration(10)
         .buildAndRegister();
 
