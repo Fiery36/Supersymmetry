f8ff899d96b95e932d244bbaf57d097752cab530
trainvoi <voitrainelephant@gmail.com>
2025-06-14 10:52:28 +0700

    fix recipe for gypsum

--- groovy/classes/ChangeFlags.groovy
+++ groovy/classes/ChangeFlags.groovy
@@ -212,6 +212,7 @@ class ChangeFlags {
         Pitchblende.setFormula("(?)UO2", true);
         Bastnasite.setFormula("(REE)CO3F", true);
         Monazite.setFormula("(REE,Th)PO4", true);
+        Gypsum.setFormula("(CaSO4)(H2O)", true);
 
         // Ore Processing
         
