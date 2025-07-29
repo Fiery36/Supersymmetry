1a72a89667244333239416a1348cc9363eae2578
ericsong1911 <ericsong1911@gmail.com>
2025-07-28 20:10:55 -0400

    fix byproduct

--- groovy/material/OrganicChemistryMaterials.groovy
+++ groovy/material/OrganicChemistryMaterials.groovy
@@ -3039,6 +3039,6 @@ class OrganicChemistryMaterials {
                 .color(0x82805b)
                 .build()
         
-        SodiumVanillylmandelateSolution.setFormula('(NaC9H7O5)(NaOH)(H2O)3', true)
+        SodiumVanillylmandelateSolution.setFormula('(NaC9H7O5)(NaOH)(H2O)4', true)
     }
 }
